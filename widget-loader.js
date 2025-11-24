2db237e5fb54d941cbb491cafb484e6d5d0f0018
testinghivesocial-dev <testinghivesocial@gmail.com>
2025-11-24 11:58:32 +0500

    Update widget-loader.js

--- widget-loader.js
+++ widget-loader.js
@@ -5,21 +5,22 @@
       console.log("Target DIV not found");
       return;
     }
-    fetch("https://clever-pithivier-04d1ab.netlify.app/widget/cars.html")
+    fetch("https://clever-pithivier-04d1ab.netlify.app/cars.html")
       .then(res => res.text())
       .then(html => {
         console.log("cars.html loaded");
         target.innerHTML = html;
         const css = document.createElement("link");
         css.rel = "stylesheet";
-        css.href = "https://clever-pithivier-04d1ab.netlify.app/widget/style.css";
+        css.href = "https://clever-pithivier-04d1ab.netlify.app/style.css";
         document.head.appendChild(css);
         console.log("CSS loaded");
         const script = document.createElement("script");
-        script.src = "https://clever-pithivier-04d1ab.netlify.app/widget/cars.js";
+        script.src = "https://clever-pithivier-04d1ab.netlify.app/cars.js";
         script.onload = () => console.log("JS loaded");
         document.body.appendChild(script);
       })
       .catch(err => console.error("Widget load error:", err));
 
   });
+
